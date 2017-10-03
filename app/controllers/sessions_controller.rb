2778b2067242e944e4cfa7fe87f3d7ae54b68e21
khun84 <khun84@gmail.com>
2017-10-03 22:25:14 +0800

    added test view for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
 class SessionsController < Clearance::SessionsController
+    skip_before_action :verify_authenticity_token, only: :create_from_omniauth
 
+    def create_from_omniauth
+
+    end
+
+    def test
+        render 'test'
+    end
 end
