4c058e4dc5873bfe674dc17e5894aede694cc004
khun84 <khun84@gmail.com>
2017-10-03 23:23:44 +0800

    implemented oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,65 @@ class SessionsController < Clearance::SessionsController
     skip_before_action :verify_authenticity_token, only: :create_from_omniauth
 
     def create_from_omniauth
+        auth_hash = request.env["omniauth.auth"]
+        if auth_hash.present?
+            # passed steam authentication
+            user = User.find_by uid: User.change_uid_to_32bit(auth_hash[:uid]), provider: auth_hash[:provider]
+
+            # first time sign in
+            if user.nil?
+                user = User.create_from_omniauth uid:auth_hash[:uid], name:auth_hash[:info][:name], country: auth_hash[:info][:location], provider: auth_hash[:provider]
+                if user.errors.messages.present?
+                    # error in user creation
+                    flash[:error] = user.errors.messages
+                    return redirect_to root_path
+                else
+                    # sign in the user and redirect them to complete their profile
+                    sign_in(user)
+                    flash[:notice] = 'Successfully sign in! Please complete your profile!'
+                    # TODO change to user profile path
+                    return redirect_to root_path
+                end
+
+            end
+
+            # existing user flow
+            sign_in(user)
+            flash[:notice] = 'Successfully sign in!'
+            return redirect_to root_path
+        else
+            # failed to authenticate steam account
+            flash[:error] = 'Please make sure you can sign in your Steam account!'
+            return redirect_to root_path
+        end
+
 
     end
 
     def test
         render 'test'
+    end
+
+    private
+
+    def redirect_signed_in_users
+        if signed_in?
+            redirect_to url_for_signed_in_users
+        end
+    end
+
+    def url_after_create
+        #  TODO to be decided
+        root_path
+    end
+
+    def url_after_destroy
+        #  TODO to be decided
+        root_path
+        # sign_in_url
+    end
+
+    def url_for_signed_in_users
+        url_after_create
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,31 @@
 class User < ApplicationRecord
   include Clearance::User
 
+    BIT_CONVERSION = 76561197960265728
+
+    def create_from_omniauth(uid:nil, name:'anonymous', country:nil, provider:nil)
+        user = User.new
+        user.uid = User.change_uid_to_32bit uid_64bit: uid
+        user.provider = provider
+        user.name = name
+        user.country = country
+        user.password = SecureRandom.hex(10)
+        user.save
+        return user
+    end
+
+    def self.find_by_uid(uid_64bit: nil)
+        self.find_by uid: self.change_uid_to_32bit(uid_64bit)
+    end
+
+
+    def self.change_uid_to_32bit(uid_64bit:nil)
+        (uid_64bit.to_i - self.bit_conversion).to_s
+    end
+
+    private
+
+    def self.bit_conversion
+        BIT_CONVERSION
+    end
 end
