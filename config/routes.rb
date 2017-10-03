c5a8b7b83588cbe2353ee7ce85df90e4108ba6ab
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-03 17:11:17 +0800

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :teams
+  resources :tournaments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
