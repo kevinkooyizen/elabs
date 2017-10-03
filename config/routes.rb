3a2d17cc145f0c02a2bc2d142057c5f2c2a6a7f6
khun84 <khun84@gmail.com>
2017-10-03 22:19:08 +0800

    added sign in routes and call back routes for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root 'static#home'
-  resources :users
-  resources :teams
-  resources :tournaments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'static#home'
+    resources :users
+    resources :teams
+    resources :tournaments
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # route to create session after steam authentication
+    match '/auth/:provider/callback', to: 'sessions#create', via: :all
+
+    get 'sign_in_steam', to: '/auth/steam', as: :sign_in_steam
 end
