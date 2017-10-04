c7533c45dc97b9bd80e135881773a7bc0531db9d
khun84 <khun84@gmail.com>
2017-10-04 11:58:56 +0800

    added users controller action access control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,38 @@
 require 'open-uri'
 class UsersController < ApplicationController
-  def new
-  end
+    include UsersHelper
+    before_action(except: [:new, :create]) do
+        if !signed_in?
+            flash[:alert] = 'Please sign in to perform this action.'
+            return redirect_to root_path
+        elsif !is_resource_owner?(params[:id])
+            flash[:alert] = 'You do not have the permission to perform this action.'
+            return redirect_to root_path
+        end
+    end
 
-  def create
-  end
+    def new
+    end
 
-  def update
-  end
+    def create
+    end
 
-  def edit
-  end
+    def update
+    end
 
-  def destroy
-  end
+    def edit
+    end
 
-  def index
-  end
+    def destroy
+    end
 
-  def show
-    user_id = 100893614
-    @user = JSON.parse open("https://api.opendota.com/api/players/#{user_id}").read
-    @user_winlose = JSON.parse open("https://api.opendota.com/api/players/#{user_id}/wl").read
-    @user_winrate = 100 * @user_winlose["win"]/(@user_winlose["win"] + @user_winlose["lose"])
-  end
+    def index
+    end
+
+    def show
+        user_id = 100893614
+        @user = JSON.parse open("https://api.opendota.com/api/players/#{user_id}").read
+        @user_winlose = JSON.parse open("https://api.opendota.com/api/players/#{user_id}/wl").read
+        @user_winrate = 100 * @user_winlose["win"]/(@user_winlose["win"] + @user_winlose["lose"])
+    end
 end
