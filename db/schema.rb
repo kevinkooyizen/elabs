8f081228d68bbaa13da68f0a89fc8e51b5eb0255
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-04 17:11:16 +0800

    adding team id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004072001) do
+ActiveRecord::Schema.define(version: 20171004072043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.string "uid"
-    t.string "token"
-    t.string "provider"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_authentications_on_user_id"
-  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -87,6 +77,8 @@ ActiveRecord::Schema.define(version: 20171004072001) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: true
+    t.integer "dota2_team_id"
   end
 
   create_table "tournaments", force: :cascade do |t|
@@ -117,7 +109,6 @@ ActiveRecord::Schema.define(version: 20171004072001) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "authentications", "users"
   add_foreign_key "participants", "teams"
   add_foreign_key "participants", "tournaments"
   add_foreign_key "players", "teams"
