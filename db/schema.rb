04039e0982d0a8195a06cc2fb8cbdc87c69d5129
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-05 11:34:50 +0800

    assigning team reference to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004072043) do
+ActiveRecord::Schema.define(version: 20171005033227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.string "uid"
-    t.string "token"
-    t.string "provider"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_authentications_on_user_id"
-  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -89,6 +79,8 @@ ActiveRecord::Schema.define(version: 20171004072043) do
     t.datetime "updated_at", null: false
     t.boolean "status", default: true
     t.integer "dota2_team_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "tournaments", force: :cascade do |t|
@@ -119,7 +111,6 @@ ActiveRecord::Schema.define(version: 20171004072043) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "authentications", "users"
   add_foreign_key "participants", "teams"
   add_foreign_key "participants", "tournaments"
   add_foreign_key "players", "teams"
@@ -128,4 +119,5 @@ ActiveRecord::Schema.define(version: 20171004072043) do
   add_foreign_key "sponsorships", "games"
   add_foreign_key "sponsorships", "sponsors"
   add_foreign_key "sponsorships", "teams"
+  add_foreign_key "teams", "users"
 end
