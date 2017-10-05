3df94bf419377815cb04208ec6a228f0a94152ee
khun84 <khun84@gmail.com>
2017-10-05 18:30:16 +0800

    change events table name to happenings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005091052) do
+ActiveRecord::Schema.define(version: 20171005102901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,17 +25,17 @@ ActiveRecord::Schema.define(version: 20171005091052) do
     t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
-  create_table "events", force: :cascade do |t|
+  create_table "games", force: :cascade do |t|
     t.string "name"
-    t.datetime "time"
-    t.string "location"
-    t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "games", force: :cascade do |t|
+  create_table "happenings", force: :cascade do |t|
     t.string "name"
+    t.datetime "time"
+    t.string "location"
+    t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
