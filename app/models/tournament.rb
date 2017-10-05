ce20249f59aa6186dc4f820eb6b44e0f96966caa
andylsh <mr.andylsh@outlook.com>
2017-10-05 11:54:03 +0800

    pending

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,16 @@
 class Tournament < ApplicationRecord
+
+	def show_tournament
+		url = URI("https://api.sportradar.us/dota2-t1/us/tournaments/sr:tournament:2464/results.xml?api_key=#{ENV['RADAR_KEY']}")
+
+		http = Net::HTTP.new(localhost, 3000)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+		request = Net::HTTP::Get.new(url)
+
+		response = http.request(request)
+		puts response.read_body
+	end	
+
 end
