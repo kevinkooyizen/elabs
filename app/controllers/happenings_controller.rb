05bc3100c32c3a25d300a14a3c23bd597cc356e7
khun84 <khun84@gmail.com>
2017-10-05 23:47:26 +0800

    refactored search helper and enhanced search form

--- app/controllers/happenings_controller.rb
+++ app/controllers/happenings_controller.rb
@@ -1,38 +1,39 @@
 class HappeningsController < ApplicationController
     include HappeningsHelper
+    include SearchHelper
+
     def index
        @happenings = Happening.all
     end
 
-    def show
-
-    end
-
     def search
-        permitted_params = search_params
         @happenings = Happening.all
         params_valid = true
+        date_from = PseudoDate.new(search_params[:"from(1i)"],search_params[:"from(2i)"],search_params[:"from(3i)"])
+        date_to = PseudoDate.new(search_params[:"to(1i)"],search_params[:"to(2i)"],search_params[:"to(3i)"])
 
-        if !Date.valid_date?(search_params[:"from(1i)"].to_i, search_params[:"from(2i)"].to_i, search_params[:"from(3i)"].to_i)
+        if !date_from.valid?
             flash[:error] = 'Invalid date!'
             params_valid = false
         end
 
-        if !Date.valid_date?(search_params[:"to(1i)"].to_i, search_params[:"to(2i)"].to_i, search_params[:"to(3i)"].to_i)
+        if !date_to.valid?
             flash[:error] = 'Invalid date!'
             params_valid = false
         end
-        
-        
+
         if params_valid
             @happenings = Happening.happening_search(name: search_params[:name],
                                                      location: search_params[:location],
-                                                     date_lower_range:
-                                                             concat_to_date(year: search_params[:"from(1i)"], month: search_params[:"from(2i)"], day: search_params[:"from(3i)"]),
-                                                    date_upper_range:
-                                                            concat_to_date(year: search_params[:"to(1i)"], month: search_params[:"to(2i)"], day: search_params[:"to(3i)"])
-                                                    )
+                                                     date_lower_range: date_from.to_string,
+                                                    date_upper_range: date_to.to_string)
         end
+
+        # this is to return the params keyed in by user when the page is refreshed
+        # can be omitted if ajax is implemented
+        @params = search_params.to_h
+        @params[:from] = date_from.valid? ? date_from.to_date : Date.today
+        @params[:to] = date_to.valid? ? date_to.to_date : Date.today
 
         render 'index'
     end
---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,30 @@
+module SearchHelper
+
+    PseudoDate = Struct.new(:yr, :mth ,:d) do
+        def year
+            yr.to_i
+        end
+
+        def month
+            mth.to_i
+        end
+
+        def day
+            d.to_i
+        end
+
+        def valid?
+            Date.valid_date?(year, month, day)
+        end
+
+        def to_date
+            Date.new(year, month, day)
+        end
+
+        def to_string
+            to_date.strftime('%Y%m%d')
+        end
+    end
+
+
+end
