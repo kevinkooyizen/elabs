bf56c5629540c993c9688545cee26e273682505b
kentlow <lowkent@live.com>
2017-10-06 22:32:57 +0800

    Fixed footer and inconsistency with bg image

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,9 +3,9 @@ class StaticController < ApplicationController
   def home
     @user = User.find_by(id: 2)
 
-    @player = Player.find_by(id: 2)
+    @player = Player.find_by(id: 1)
 
-    @team = Team.find_by(id: 2)
+    @team = Team.find_by(id: 1)
 
   end
 
