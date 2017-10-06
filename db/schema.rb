7da1605fb0c8ff2060b86e414916538843cdad0e
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-06 15:55:12 +0800

    added rating to team database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006073349) do
+ActiveRecord::Schema.define(version: 20171006074926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20171006073349) do
     t.bigint "user_id"
     t.float "winrate"
     t.text "roster", default: [], array: true
+    t.integer "rating"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
