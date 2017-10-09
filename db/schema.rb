8a221708cd669e1e4682abe626e923e5c83a6ba8
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-09 12:52:11 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008102327) do
+ActiveRecord::Schema.define(version: 20171009042342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20171008102327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "api_npc_name"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "api_id"
+    t.string "api_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "participants", force: :cascade do |t|
