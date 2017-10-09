6708c06989b838a8fd7485053df9786defe9deff
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-09 18:17:57 +0800

    static team fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,7 @@ class StaticController < ApplicationController
   def home
     @user = User.first
     @player = Player.first
-    @team = Team.find(17)
+    @team = Team.first
 
     @tournaments = Tournament.display_tournaments.page(params[:page]).per(3)
   end
