8960ab47c079936d8359ca39a39a24fcfcc40ee6
Koo Yi Zen <kooyizen@hotmail.com>
2017-10-10 20:33:04 +0800

    remove mmr and player status from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010115507) do
+ActiveRecord::Schema.define(version: 20171010122946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.string "uid"
-    t.string "token"
-    t.string "provider"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_authentications_on_user_id"
-  end
 
   create_table "enquiries", force: :cascade do |t|
     t.bigint "user_id"
@@ -34,15 +24,6 @@ ActiveRecord::Schema.define(version: 20171010115507) do
     t.string "status"
     t.index ["team_id"], name: "index_enquiries_on_team_id"
     t.index ["user_id"], name: "index_enquiries_on_user_id"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.datetime "time"
-    t.string "location"
-    t.text "detail"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "games", force: :cascade do |t|
@@ -203,13 +184,10 @@ ActiveRecord::Schema.define(version: 20171010115507) do
     t.string "uid"
     t.string "state"
     t.string "persona_name"
-    t.boolean "player_status", default: false
-    t.integer "mmr", default: 0
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "authentications", "users"
   add_foreign_key "enquiries", "teams"
   add_foreign_key "enquiries", "users"
   add_foreign_key "members", "teams"
