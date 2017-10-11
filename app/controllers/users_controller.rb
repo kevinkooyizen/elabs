30feecaefaa482c210a60ba9e2766cf88c6ca947
andylsh <mr.andylsh@outlook.com>
2017-10-11 14:45:56 +0800

    Fixed some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,11 @@ class UsersController < ApplicationController
         @var = user
         @var.store
         @teams = Enquiry.where(user_id: params[:id], status: "user")
-        @team = Team.find_by(user_id: current_user.id)
+        if signed_in?
+            @team = Team.find_by(user_id: current_user.id)
+        else
+            @team = Team.find_by(user_id: User.find(params[:id]))
+        end
         # if @team.nil?
         #     if !Player.find_by(user_id: User.find(params[:id]).id).nil?
         #         if !Player.find_by(user_id: User.find(params[:id]).id).team_id.nil?
